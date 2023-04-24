c381ead933eaebe8fbe8769fc338e6593c23ca1d
Omar Ouaziz <63248283+zaynOm@users.noreply.github.com>
2023-04-24 21:26:36 +0000

    add number conversion specifiers

---
+++ print_nums.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * op_int_helper - prints a number
+ * @n: integer
+ * 
+ * Return: the length of @n
+*/
+int op_int_helper(int n)
+{
+    int len = 0;
+
+    if (n / 10)
+        len += op_int_helper(n / 10);
+
+    _putchar((n % 10) + '0');
+    len++;
+
+    return (len);
+}
+
+/**
+ * op_int - prints an integer
+ * @vl: varasic list of arguments
+ * 
+ * Return: the length of the argument
+*/
+int op_int(va_list vl)
+{
+    int len = 0;
+    int n = va_arg(vl, int);
+
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+        len++;
+    }
+
+    if (n / 10)
+        len += op_int_helper(n);
+
+    return (len);
+}
