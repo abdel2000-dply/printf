11e84a497ae0566b4a05c48e9ccfd91e1109c9bd
zaynOm <ouaziz0626@gmail.com>
2023-04-25 04:15:52 +0100

    fixing betty issues

--- print_base.c
+++ print_base.c
@@ -5,20 +5,20 @@
  * @n: number to convert
  * @base: base to use
  * @c: char
- * 
+ *
  * Return: the length of the printed number
-*/
+ */
 int convert_base(unsigned int n, int base, char c)
 {
-    int len = 0;
-    
+	int len = 0;
+
 	if (n / base)
 		len += convert_base(n / base, base, c);
 
-    if (n % base > 9)
-        len += _putchar(n % base + c);
-    else
-	    len += _putchar((n % base) + '0');
+	if (n % base > 9)
+		len += _putchar(n % base + c);
+	else
+		len += _putchar((n % base) + '0');
 
 	return (len);
 }
@@ -26,7 +26,7 @@ int convert_base(unsigned int n, int base, char c)
 /**
  * op_octal - prints num in octal base
  * @vl: varasic list of arguments
- * 
+ *
  * Return: the length of the argument
  */
 int op_octal(va_list vl)
@@ -45,7 +45,7 @@ int op_octal(va_list vl)
 int op_hex(va_list vl)
 {
 	long int n = va_arg(vl, unsigned int);
-    
+
 	return (convert_base(n, 16, 87));
 }
 
@@ -58,6 +58,6 @@ int op_hex(va_list vl)
 int op_heX(va_list vl)
 {
 	long int n = va_arg(vl, unsigned int);
-    
+
 	return (convert_base(n, 16, 55));
 }
