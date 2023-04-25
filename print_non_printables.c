ec03920b1169bc6227395114f1a5f956bd9e0d43
Omar Ouaziz <63248283+zaynOm@users.noreply.github.com>
2023-04-25 13:54:08 +0000

    create %S custom conversion specifier

---
+++ print_non_printables.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * op_nptbl - prints non printable chars in a string
+ * @vl: ...
+ * Return: ...
+ */
+int op_nptbl(va_list vl)
+{
+	int i = 0, len = 0;
+	char *str = va_arg(vl, char *);
+
+	if (!str)
+		str = "(null)";
+
+	while (str[i])
+    {
+        if (0 < str[i] && str[i] < 32 || 127 <= str[i])
+		{
+            len += (str[i] < 16) ? _printf("\\x0") : _printf("\\x");
+			len += convert_base(str[i], 16, 55);
+            i++;
+		}
+        if (!str[i])
+		    return (len);
+
+            len += _putchar(str[i]);
+        i++;
+    }
+
+	return (len);
+}
