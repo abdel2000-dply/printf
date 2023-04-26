3215d2d5e6566170dabb34b2d6dbf0efad2c86c1
zaynOm <ouaziz0626@gmail.com>
2023-04-26 01:28:49 +0100

    fixing betty issues

--- print_reverse.c
+++ print_reverse.c
@@ -6,21 +6,21 @@
  * @flag: flag
  *
  * Return: the length of the printed chars
-*/
+ */
 int op_reverse(va_list vl, int flag)
 {
-    int i = 0, len = 0;
-    char *str = va_arg(vl, char *);
-    (void) flag;
+	int i = 0, len = 0;
+	char *str = va_arg(vl, char *);
+	(void) flag;
 
-    if (str == NULL)
-        str = "(NULL)";
+	if (str == NULL)
+		str = "(NULL)";
 
-    while (str[len])
-        len++;
+	while (str[len])
+		len++;
 
-    for (i = len - 1; i >= 0; i--)
-        _putchar(str[i]);
+	for (i = len - 1; i >= 0; i--)
+		_putchar(str[i]);
 
-    return (len);
+	return (len);
 }
