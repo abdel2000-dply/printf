7741e27d44c72591bd4ff53bfcf974bbdc35acf1
Omar Ouaziz <63248283+zaynOm@users.noreply.github.com>
2023-04-26 00:43:59 +0000

    adding (AHYY) for (null)

--- print_rot13.c
+++ print_rot13.c
@@ -15,6 +15,9 @@ int op_rot13(va_list vl, int flag)
 	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
     (void) flag;
 
+    if (str == NULL)
+		str = "(AHYY)";
+        
 	for (; s[i]; i++)
 	{
 		j = 0;
