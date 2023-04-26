cdcc5f2e7526606949e0fd63b01c83a838b8bb1c
Omar Ouaziz <63248283+zaynOm@users.noreply.github.com>
2023-04-26 00:38:33 +0000

    adding rot13 functionality

---
+++ print_rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @vl: string to encode
+ * @flag: ...
+ *
+ * Return: the length of printed string
+ */
+int op_rot13(va_list vl, int flag)
+{
+	int i = 0, j;
+    char *s = va_arg(vl, char *);
+	char alph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+    (void) flag;
+
+	for (; s[i]; i++)
+	{
+		j = 0;
+		for (; alph[j]; j++)
+		{
+			if (s[i] == alph[j])
+			{
+				_putchar(rot13[j]);
+				break;
+			}
+		}
+
+	}
+	return (i);
+}
